622fd673f7818ef854487f33c436cd013ea16a3e
jrpbaroro <78065292+jrpbaroro@users.noreply.github.com>
2022-06-23 17:23:34 +0800

    Add initial commit

---
+++ js/grades.js
@@ -0,0 +1,24 @@
+var items = $(".list-wrapper .list-item");
+var numItems = items.length;
+var perPage = 1;
+
+items.slice(perPage).hide();
+
+$('#pagination-container').pagination({
+	items: numItems,
+	itemsOnPage: perPage,
+	prevText: "&laquo;",
+	nextText: "&raquo;",
+	onPageClick: function (pageNumber) {
+		var showFrom = perPage * (pageNumber - 1);
+		var showTo = showFrom + perPage;
+		items.hide().slice(showFrom, showTo).show();
+	}
+});
+
+
+
+
+
+
+
